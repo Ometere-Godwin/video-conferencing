4c8345c6c404aafa1cd598dafd446bcdee7f2803
Ometere-Godwin <ookuoluse@yahoo.com>
2024-06-27 14:21:34 +0100

    Upcoming and previous meetings

--- constants/index.ts
+++ constants/index.ts
@@ -1,33 +1,41 @@
 import SVGShape from "@/components/SVGShape";
 
-export const sidebarLinks =[
-    {
-        label: "Home",
-        route: "/",
-        imgUrl: "/icons/home-icon.svg",
-    },
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/home-icon.svg",
+  },
 
-    {
-        label: "Upcoming",
-        route: "/upcoming",
-        imgUrl: "/icons/upcoming-icon.svg",
-    },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming-icon.svg",
+  },
 
-    {
-        label: "Previous",
-        route: "/previous",
-        imgUrl: "/icons/calendar-icon.svg",
-    },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/calendar-icon.svg",
+  },
 
-    {
-        label: "Recordings",
-        route: "/recordings",
-        imgUrl: "/icons/video-icon.svg",
-    },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/video-icon.svg",
+  },
 
-    {
-        label: "Personal Room",
-        route: "/personal-room",
-        imgUrl: "/icons/addbutton-icon.svg",
-    },
-]
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/addbutton-icon.svg",
+  },
+];
+
+export const avatarImages = [
+  "/images/avatar-1.jpeg",
+  "/images/avatar-2.jpeg",
+  "/images/avatar-3.png",
+  "/images/avatar-4.png",
+  "/images/avatar-5.png",
+];
